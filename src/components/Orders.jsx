952eec7e41628e06fa53dd5cff16eb0cc606ac2b
김하경 <khg05311@naver.com>
2021-12-29 12:32:01 +0900

    Order component making process

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,8 +1,44 @@
+import useOrders from "../hooks/useOrders";
+import usePrototypes from "../hooks/usePrototypes";
+
 export default function Orders() {
-  return <aside>
-    <div className="empty">
-      <div className="title">You don't have any orders</div>
-      <div className="subtitle">Click on a + to add an order</div>
+  const orders = useOrders();
+  const prototypes = usePrototypes();
+
+  if(orders.length == 0){
+    return (
+      <aside>
+        <div className="empty">
+          <div className="title">You don't have any orders</div>
+          <div className="subtitle">Click on a + to add an order</div>
+        </div>
+      </aside>
+    );
+  }
+
+  return <aside> 
+    <div className="body">
+      {orders.map(order => {
+        const {id} = order;
+        const prototype = prototypes.find(p => p.id == id)
+        return(<div className="item" key={id}>
+          <div className="img">
+            <video src={prototype.thumbnail} />
+          </div>
+          <div className="content">
+            <p className="title">
+              {prototype.title} X {order.quantity}
+            </p>
+          </div>
+          <div className="action">
+            <p className="price"> $ {prototype.price * order.quantity} </p>
+            <button className="btn btn--link" >
+              <i className="icon icon--cross" />
+            </button>
+          </div>
+        </div>)
+      })}
     </div>
   </aside>
+  
 }
